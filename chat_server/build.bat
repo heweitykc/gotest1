8e8fd11e95aa6f53da146eb15d3edcd0053f9b53
callee <heweitykc@163.com>
2016-06-20 10:23:56 +0800

    1

---
+++ chat_server/build.bat
@@ -0,0 +1,5 @@
+SET GOPATH=%~dp0
+go install server
+cd bin 
+server.exe
+pause
---
+++ chat_server/installpkg.bat
@@ -0,0 +1,4 @@
+SET GOPATH=%~dp0
+go get github.com/name5566/leaf
+go get github.com/golang/protobuf/proto
+go get gopkg.in/mgo.v2
