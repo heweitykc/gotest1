e0f976176ac5cf53517acc446e166a08ca4c88d9
callee <heweitykc@163.com>
2016-06-20 15:24:10 +0800

    1

--- chat_server/installpkg.bat
+++ chat_server/installpkg.bat
@@ -1,5 +1,8 @@
 SET GOPATH=%~dp0
-go get github.com/name5566/leaf
+
 go get github.com/golang/protobuf/proto
+go install github.com/golang/protobuf/protoc-gen-go
 go get gopkg.in/mgo.v2
 go get github.com/go-sql-driver/mysql
+
+go get github.com/name5566/leaf
