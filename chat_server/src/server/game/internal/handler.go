c5215a85b305e1bd50d8ee064a5f62579b63a161
callee <heweitykc@163.com>
2016-06-27 12:08:39 +0800

    1

--- chat_server/src/server/game/internal/handler.go
+++ chat_server/src/server/game/internal/handler.go
@@ -1,14 +1,20 @@
 package internal
 
 import (
+	"net/rpc"
 	"fmt"
 	"github.com/name5566/leaf/gate"
 	"reflect"
 	"server/msg"
 	"time"
+	"log"
 )
 
 const maxMessages = 50
+
+type Args struct {
+        A, B int
+}
 
 var (
 	messages [maxMessages]struct {
@@ -16,6 +22,7 @@ var (
 		message  string
 	}
 	messageIndex int
+	rpcClient *rpc.Client
 )
 
 var loc = time.FixedZone("", 8*3600)
@@ -25,6 +32,11 @@ func handleMsg(m interface{}, h interface{}) {
 }
 
 func init() {
+	rpcclient, rcperr := rpc.DialHTTP("tcp", "127.0.0.1" + ":1234")
+	if rcperr != nil {
+			log.Fatal("dialing:", rcperr)
+	}
+	rpcClient = rpcclient
 	handleMsg(&msg.C2S_AddUser{}, handleAddUser)
 	handleMsg(&msg.C2S_Message{}, handleMessage)
 	handleMsg(&msg.C2S_Action{}, handleAction)
@@ -55,6 +67,14 @@ func handleAddUser(args []interface{}) {
 		UserName: m.UserName,
 		NumUsers: int32(len(users)),
 	}, a)
+	
+	rpcargs := &Args{7,8}
+	reply := make([]string, 10)
+	err := rpcClient.Call("Arith.Multiply", rpcargs, &reply)
+	if err != nil {
+			log.Fatal("arith error:", err)
+	}
+    log.Println(reply)
 }
 
 func handleMessage(args []interface{}) {
--- db_server/build.bat
+++
@@ -1,5 +0,0 @@
-SET GOPATH=%~dp0
-go install dbserver
-cd bin 
-dbserver.exe
-pause
